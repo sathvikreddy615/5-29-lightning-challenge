f55f5caccf5dc17e9d394faf32645221cf6e52de
Sathvik Reddy <sathvik.reddy615@gmail.com>
2018-06-04 10:17:41 -0500

    completed and uploaded challenge to GitHub

---
+++ main.js
@@ -0,0 +1,11 @@
+let nss = ["exciting", "fun", "interesting", "complex", "helpful"];
+
+const nssEXP = () => {
+  for (let i = 0; i < nss.length; i++) {
+    console.log(`My NSS experience has been ${nss[i]}`);
+  }  
+}
+
+nssEXP();
+
+
